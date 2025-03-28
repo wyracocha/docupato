c12aa92dbbcc0a5363b7138f292b9fa5c0cfd48e
Miguel Aliaga <maliaga.pantoja@gmail.com>
2025-03-28 15:06:19 -0500

    updating script output name

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -3,6 +3,6 @@ CONTENT=$($PWD/scripts/requests.sh | jq .output[0].content[0].text)
 echo -e $CONTENT > /tmp/out.txt
 # removing code separators
 sed '1,1d' /tmp/out.txt  >> /tmp/out2.txt
-sed '$d' /tmp/out2.txt  >> out.txt
+sed '$d' /tmp/out2.txt  >> out.mmd
 # building image from .mmd
 podman run --rm -u $(id -u):$(id -g) -v $PWD:/data:z --userns keep-id ghcr.io/mermaid-js/mermaid-cli/mermaid-cli -i out.mmd
